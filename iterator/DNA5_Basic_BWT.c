7d020f5ec12933bfb0bbcac34e0bff7f2cc7477c
alowayyeds <alowayyeds@gmail.com>
2021-06-12 11:18:44 +0300

    remove useDivsufsort()

--- iterator/DNA5_Basic_BWT.c
+++ iterator/DNA5_Basic_BWT.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include "DNA5_Basic_BWT.h"
 #include "../io/bits.h"
-#include "divsufsort64.h"
 
 
 BwtIndex_t *newBwtIndex() {
@@ -20,40 +19,6 @@ void freeBwtIndex(BwtIndex_t *Basic_BWT) {
 		}
 		free(Basic_BWT);
 	}
-}
-
-
-/**
- * Builds the BWT of T# from the suffix array of T built by divsufsort.
- * 
- * @param text the string T (without the final sharp), of length $length$;
- * @param Basic_BWT to set the position of the sharp;
- * @return a pointer to the BWT, or NULL if construction failed.
- */
-static inline uint8_t *useDivsufsort(char *text, uint64_t length, BwtIndex_t *Basic_BWT) {
-	uint32_t error;
-	uint64_t i, textPosition;
-	uint8_t *bwt = NULL;
-	int64_t *suffixArray = NULL;
-	
-	suffixArray=(int64_t *)malloc(length*sizeof(int64_t));
-	error=divsufsort64((uint8_t *)text,suffixArray,length);
-	if (error) {
-		free(suffixArray);
-		return NULL;
-	}
-	bwt=(uint8_t *)malloc(length+1);
-	bwt[0]=text[length-1];
-	for (i=0; i<length; i++) {
-		textPosition=suffixArray[i];
-		if (textPosition==0) {
-			Basic_BWT->sharpPosition=i+1;		
-			bwt[i+1]=DNA_ALPHABET[0];
-		}
-		else bwt[i+1]=text[textPosition-1];
-	}
-	free(suffixArray);
-	return bwt;
 }
 
 
