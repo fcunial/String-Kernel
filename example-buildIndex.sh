0367fde968bd4b676c49d0b88b07ea9193cba607
alowayyeds <alowayyeds@gmail.com>
2021-06-23 14:41:47 +0300

    Remove .sh files

--- example-buildIndex.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-INPUT_FILE="../data/HS22.fasta"
-INPUT_FORMAT="1"
-APPEND_RC="0"
-OUTPUT_FILE="${INPUT_FILE}.indexed"
-
-./buildIndex ${INPUT_FILE} ${INPUT_FORMAT} ${APPEND_RC} ${OUTPUT_FILE}
